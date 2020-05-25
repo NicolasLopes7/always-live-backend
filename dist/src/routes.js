a0b2e90ac9cfe3c8eb76c760bcdc53a6f2d83914
Nicolas Lopes Aquino <nicolaslopesbusiness@gmail.com>
2020-05-25 10:36:19 -0300

    Changing DevDependencies

--- dist/src/routes.js
+++ dist/src/routes.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var SiteController_1 = __importDefault(require("./Controllers/SiteController"));
-// eslint-disable-next-line import/no-unresolved
 var PingToAll_1 = __importDefault(require("./services/PingToAll"));
 var routes = express_1.Router();
 routes.post('/addSite', SiteController_1.default.store);
